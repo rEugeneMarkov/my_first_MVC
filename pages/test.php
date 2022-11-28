46307f2e8929503e287fb5478366ff35d248be32
rEugeneMarkov <117519379+rEugeneMarkov@users.noreply.github.com>
2022-11-28 02:55:44 +0200

    test

--- pages/test.php
+++ pages/test.php
@@ -1,12 +1,16 @@
 <?php
 
 require $_SERVER['DOCUMENT_ROOT'] . "/templates/header.php";
+spl_autoload_register(function ($class_name) {
+    $a = explode('\\', $class_name);
+    $last = array_pop($a);
+    $fn = $last . '.php';
+    require $fn;
+});
 
-require 'test2.php';
-//require 'config.php';
+$email = $_SESSION['email'] ?? '';
+$userdata = get_user($email);
 
-    $email = $_SESSION['email'] ?? '';
-    $userdata = get_user($email);
 $user = new \First\User($userdata['name'], $userdata['email'], $userdata['pass']);
 //$user->name = $userdata['name'];
 //echo $user->getUserName();
